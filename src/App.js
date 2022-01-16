f7f832d2406951f9840a49e5e3964c17dac2938b
unknown <benklinton@gmail.com>
2022-01-15 22:17:49 -0700

    set up front end

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import './style.css';
+import { Container, Form, Button } from 'react-bootstrap';
+import { render } from '@testing-library/react';
 
-function App() {
+class App extends React.Component {
+
+  state = {
+    hours: ''
+  }
+
+
+  handleChange(e) {
+    this.setState({hours: e.target.value});
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log(this.state)
+  }
+
+render() {
   return (
-   <>
-   </>
+   <Container>
+     <h1 className='text-center margin-top'>Please enter your play time for Hunt:Showdown</h1>
+     <h3 className='text-center'>Round to the nearest hour</h3>
+     <Form className="form-center" onSubmit={this.handleSubmit}>
+       <Form.Control className="input-style" type='text' value={this.state.hours} onChange={this.handleChange.bind(this)}></Form.Control>
+       <Button type="submit" value='submit' variant="outline-secondary" className="button-style">Submit</Button>
+     </Form>
+
+   </Container>
   );
 }
+}
+
 
 export default App;
